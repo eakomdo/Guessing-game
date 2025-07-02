e04eb80b58dbb05aff767d94afdca40ea0e9ebfd
eakomdo <akomdoemmanuel22@gmail.com>
2025-07-02 12:23:47 +0000

    Created a guessing game

---
+++ guessgame.py
@@ -0,0 +1,24 @@
+#A guessing game  where the user guesses a secret number
+
+secret_number = 7
+
+guess_count = 0
+max_guesses = 3
+
+while guess_count < max_guesses:
+    guess = int(input("Guess the secret number (between 1 and 10): "))
+    
+    if guess < 1 or guess > 10:
+        print("Please guess a number between 1 and 10.")
+        continue
+    
+    guess_count += 1
+    
+    if guess == secret_number:
+        print("Congratulations! You've guessed the secret number!")
+        continue 
+    else:
+        print("Wrong guess. Try again.")
+        break
+else:
+    print("Sorry, you've used all your guesses. The secret number was", secret_number)
